f855210e2fdcc5e31139c5e4a93802552e7d3cc0
MaryaSamsonenko <84477591+MaryaSamsonenko@users.noreply.github.com>
2021-10-19 18:27:30 +0300

    убрала лишний код, т.к. у этой библиотеки свои встроенные настойки))))))))

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,37 +8,21 @@ const galleryEl = document.querySelector(".gallery");
 const arrayEl = galleryItems.reduce(
   (acc, { preview, original, description }) =>
     acc +
-    `
-  <a class="gallery__item" href="${original}">
+    `<li><a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
       alt="${description}"
     />
-  </a>
+  </a></li>
 `,
   ""
 );
 galleryEl.insertAdjacentHTML("afterbegin", arrayEl);
 
-galleryEl.addEventListener("click", onGetLargeImage);
-
-function onGetLargeImage(event) {
-  event.preventDefault();
-  if (!event.target.classList.contains("gallery__image")) {
-    return;
-  }
-  const currentImageUrl = event.target.dataset.source;
-  openModal(currentImageUrl);
-}
 const optionsForModal = {
   captionsData: "alt",
   captionDelay: 250,
 };
 
-function openModal(currentImageUrl) {
-  const modalFromLib = new SimpleLightbox(".gallery__item", optionsForModal);
-  modalFromLib.on("closed.simplelightbox", function () {
-    modalFromLib.refresh();
-  });
-}
+const modalFromLib = new SimpleLightbox(".gallery__link", optionsForModal);
